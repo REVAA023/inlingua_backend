45aacd9072410586276d144e5a4e3a57b0f2f893
Vignesh <157564464+REVAA023@users.noreply.github.com>
2025-04-13 23:35:25 +0530

    database

--- inlingua_backend/settings.py
+++ inlingua_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from corsheaders.defaults import default_headers
 import os
+from inlingua_backend.database import DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,16 +83,8 @@ WSGI_APPLICATION = 'inlingua_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'inlingua_database',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST':'localhost',
-        'PORT':'3306',
-    }
-}
+DATABASES = DATABASES = DATABASES
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
